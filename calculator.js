8941ceb19915ab0d3ec4b2593a49f8663f123b31
Aldrian Almonte <aldrianalmonte02@gmail.com>
2022-04-16 09:02:23 -0700

    Final push

--- calculator.js
+++ calculator.js
@@ -1,4 +1,43 @@
-function calculator(){
+let num1 = document.getElementById("number0").value;
+let num2 = document.getElementById("number1").value;
+    
+    
+function add(){
     console.log("Calculating...");
-    let num1 = Number(prompt(`Enter the number`));
-    document.getElementById("calculationZone").innerHTML=`<p>${num1} * 10 = ${num1*10}`}
+    
+
+    result = num1 + num2;
+
+    
+    document.getElementById("calculationZone").innerHTML=`<p>${num1} + ${num2} = ${result}`
+}
+
+ 
+
+ function subtract(){
+    console.log("Calculating...");
+   
+
+    result = num1 - num2;
+
+    document.getElementById("calculationZone").innerHTML=`<p>${num1} - ${num2} = ${result}`
+}
+
+function multiply(){
+    console.log("Calculating...");
+   
+
+    result = num1 * num2;
+
+    document.getElementById("calculationZone").innerHTML=`<p>${num1} * ${num2} = ${result}`
+}
+
+function divide(){
+    console.log("Calculating...");
+  
+
+    result = num1 / num2;
+
+    document.getElementById("calculationZone").innerHTML=`<p>${num1} / ${num2} = ${result}`
+}
+
